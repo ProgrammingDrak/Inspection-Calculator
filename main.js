13447d8909a87d6fa52984857be03f35174ff1f5
ProgrammingDrak <Programming.DrakeShadwell@gmail.com>
2024-04-09 21:54:28 -0400

    Updates. Still need to figure out autocomplete API

--- main.js
+++ main.js
@@ -24,3 +24,18 @@ function calculatePrice() {
 
     document.getElementById('result').innerHTML = `Estimated Price: $${price.toFixed(2)}`;
 }
+
+function initAutocomplete() {
+    // Create the autocomplete object, restricting the search to geographical location types.
+    var autocomplete = new google.maps.places.Autocomplete(
+        /** @type {!HTMLInputElement} */ (
+            document.getElementById('propertyAddress')), {
+                types: ['geocode']
+            });
+
+    // When the user selects an address from the dropdown, populate the address fields in the form.
+    autocomplete.addListener('place_changed', function() {
+        var place = autocomplete.getPlace();
+        console.log(place); // You can extract other details from the 'place' object.
+    });
+}
